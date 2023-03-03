a95476e0e902eb596200053dfedb798df5ec4f08
SaurabhNair239 <58506467+SaurabhNair239@users.noreply.github.com>
2023-03-03 10:36:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import api_data_daily
 import s3_op
 import amazon_ts
-from flask import Flask,render_template,request
+#from flask import Flask,render_template,request
 #get data from API and store it into repo
 
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def home():
+#@app.route('/',methods=['GET'])
+#def home():
+#
+#    api_data_daily.import_data_from_api()
 
+    #upload the data from local system to S3 bucket
+#    s3_op.dump_file_to_s3("final_data.csv")
+
+    #run lstm model
+ #   amazon_ts.amazon_prediction()
+
+    #push all prediction file to S3
+
+  #  s3_op.dump_file_to_s3("fut_data.csv")
+
+if __name__ == '__main__':
     api_data_daily.import_data_from_api()
 
     #upload the data from local system to S3 bucket
@@ -21,6 +34,4 @@ def home():
     #push all prediction file to S3
 
     s3_op.dump_file_to_s3("fut_data.csv")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    
