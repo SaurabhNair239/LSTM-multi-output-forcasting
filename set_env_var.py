7559becdb479558283b851bad0a1a16921002701
SaurabhNair239 <58506467+SaurabhNair239@users.noreply.github.com>
2023-03-07 09:45:21 +0100

    Update set_env_var.py

--- set_env_var.py
+++ set_env_var.py
@@ -1,8 +1,8 @@
 import os
 
 # get emails and password from environment variables
-aws_access_key_id = os.environ.get("aws_access_key_id")
-aws_secret_access_key = os.environ.get("aws_secret_access_key")
+aws_access_key_id = os.environ.get("aws_access_key_id_val")
+aws_secret_access_key = os.environ.get("aws_secret_access_key_val")
 
 
 print("aws_access_key_id: ",aws_access_key_id)
